9cf591da5de0be18263c81f81f8df527996101e6
TurnTheKeys <166112225+TurnTheKeys@users.noreply.github.com>
2024-06-04 18:43:19 +1000

    Commence Object Orientation Work
    Started converting the obstacles into classes.
    Created generic obstacle class and commenced work into creating the 'flying egg' obstacle class

---
+++ FixCharacterRunningGame/Obstacle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FixCharacterRunningGame
+{
+    public class Obstacle
+    {
+        public PictureBox ObstacleSprite { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public int ObstacleSpeed { get; set; }
+        public int ObstacleSpeedSine { get; set; }
+        public int ObstacleHeightSine { get; set; }
+        public string ObstacleName { get; set; }
+        public string ObstacleTag { get; set; }
+
+        public Obstacle(string obstacleName, int positionX, int positionY, int obstacleSpeed,int ObstacleSpeedSineEnter, int obstacleHeightSineEnter, string obstacleTag, string obstacleSpriteFilePath)
+        {
+            ObstacleName = obstacleName;
+            PositionX = positionX;
+            PositionY = positionY;
+            ObstacleSpeed = obstacleSpeed;
+            ObstacleSpeedSine = ObstacleSpeedSineEnter;
+            ObstacleHeightSine = obstacleHeightSineEnter;
+            ObstacleTag = obstacleTag;
+
+            ObstacleSprite = new PictureBox();
+            ObstacleSprite.BackColor = Color.Transparent;
+            ObstacleSprite.Tag = obstacleTag;
+            ObstacleSprite.Name = obstacleName;
+            ObstacleSprite.Image = Image.FromFile(obstacleSpriteFilePath);
+            ObstacleSprite.Location = new Point(positionX, positionY);
+        }
+
+        public virtual void Movement(int score)
+        {
+            ObstacleSprite.Left -= ObstacleSpeed;
+        }
+
+    }
+}
---
+++ FixCharacterRunningGame/ObstacleFlyingEgg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixCharacterRunningGame
+{
+    public class FlyingEgg : Obstacle
+    {
+        public FlyingEgg(int positionX, int positionY, int obstacleSpeed, int ObstacleSpeedSineEnter, int obstacleHeightSineEnter)
+            : base("flyingEgg", positionX, positionY, obstacleSpeed, ObstacleSpeedSineEnter, obstacleHeightSineEnter,  "Obstacles", "path_to_your_image_file")
+        {
+        }
+
+        public override void Movement(int score)
+        {
+            if (score > 10)
+            {
+                ObstacleSprite.Left -= (ObstacleSpeed + 2);
+                ObstacleSprite.Top = (int)(Math.Sin((double)(ObstacleSprite.Left + ObstacleSprite.Width) / ObstacleSpeedSine) * ObstacleHeightSine) + 200;
+            }
+        }
+    } 
+}
